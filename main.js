737a91be30ca3617cbf8be8624b214a71848b6f4
Sinthuvel <sinthuvel96@gmail.com>
2021-08-09 11:37:06 +0530

    Sound Cloud

---
+++ main.js
@@ -0,0 +1,24 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function(){
+
+  // jQuery methods go here...
+    SC.stream('/tracks/278148856',function(sound){ 
+
+      $('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+
+         $('#stop').click(function(e) {
+                  e.preventDefault();
+                  sound.stop();
+                });
+
+      });
+
+
+});
